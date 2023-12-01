5ab894de2112932cc1264d5549a74d210d8ed6c9
lesnich <kolesnichenko.sophia@gmail.com>
2023-12-01 06:43:16 +0100

    fixed bugs

--- src/js/get.js
+++ src/js/get.js
@@ -2,10 +2,13 @@
 const getUrl = 'https://randomuser.me/api/?results=55';
 
 module.exports = fetch(getUrl, {
+  //  HTTP-метод GET для отримання даних.
   method: 'GET',
   headers: {
+    // заголовок Content-Type як 'application/json'. 
     'Content-Type': 'application/json',
   },
-})
+}) // коли отримано відповідь від сервера, викликаємо метод json().
   .then((response) => response.json())
+  // повертаємо результати з об'єкта відповіді.
   .then((response) => response.results);
